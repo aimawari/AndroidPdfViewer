b4f156073fc7f04dac13bb6b987a3a6ffe34ef93
aimawari <aimawari.ai@gmail.com>
2019-11-18 15:34:31 +0700

    Update PDFView.java

--- android-pdf-viewer/src/main/java/com/github/barteksc/pdfviewer/PDFView.java
+++ android-pdf-viewer/src/main/java/com/github/barteksc/pdfviewer/PDFView.java
@@ -293,8 +293,8 @@ public class PDFView extends RelativeLayout {
         // assumes swiping horizontal and snapping centre
 
         page = pdfFile.determineValidPageNumberFrom(page);
-        float offset = page == 0 ? 0 : -pdfFile.getPageOffset(page, getZoom());
-        offset += pdfFile.getPageSpacing(page, getZoom()) / 2f;
+        float offset = page == 0 ? 0 : -pdfFile.getPageOffset(page, zoom);
+        offset += pdfFile.getPageSpacing(page, zoom) / 2f;
 
         if (withAnimation) {
             animationManager.startXAnimation(currentXOffset, offset);
